a14ea2b4d9347611aa4f3e5770a966f3dc4c7544
Abhay <abhaytyagi2513@gmail.com>
2023-07-05 22:34:46 +1000

    progressive commit

---
+++ scripts/NotesView.js
@@ -0,0 +1,70 @@
+export default class NotesView {
+	constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
+		this.root = root;
+		this.onNoteSelect = onNoteSelect;
+		this.onNoteAdd = onNoteAdd;
+		this.onNoteEdit = onNoteEdit;
+		this.onNoteDelete = onNoteDelete;
+		this.root.innerHTML = `
+			<div class="notes__sidebar">
+				<button class="notes__add" type="button">Add Note</button>
+				<div class="notes__list"></div>
+			</div>
+			<div class="notes__preview">
+				<input class="notes__title" type="text" placeholder="New Note">
+				<textarea class="notes__body">Take note...</textarea>
+			</div>
+		`;
+
+		const btnAddNote = this.root.querySelector(".notes__add");
+		const inpTitle = this.root.querySelector(".notes__title");
+		const inpBody = this.root.querySelector(".notes__body");
+
+		btnAddNote.addEventListener("click", () => {
+			this.onNoteAdd();
+		});
+
+		[inpTitle, inpBody].forEach(inputField => {
+			inputField.addEventListener("blur", () => {
+				const updatedTitle = inpTitle.value.trim();
+				const updatedBody = inpBody.value.trim();
+
+				this.onNoteEdit(updatedTitle, updatedBody);
+			});
+		});
+	}
+
+	_createListItemHTML(id, title, body, updated) {
+		const MAX_BODY_LENGTH = 60;
+
+		return `
+			<div class="notes__list-item" data-note-id="${id}">
+				<div class="notes__small-title">"${title}"</div>
+				<div class="notes__small-body">
+					${body.substring(0, MAX_BODY_LENGTH)}
+					${body.length > MAX_BODY_LENGTH ? "..." : ""}
+				</div>
+				<div class="notes__small-updated">
+					${updated.toLocaleString(undefined, {dateStyle: "full", timeStyle: "short"})}
+				</div>
+			</div>
+		`;
+	}
+
+	udpateNoteList(notes) {
+		const notesListContainer = this.root.querySelector(".notes__list");
+
+		notesListContainer.innerHTML = "";
+
+		for (const note of notes) {
+			const html = this._createListItemHTML(note.id, note.title, note.body, new Date(note.updated));
+			notesListContainer.insertAdjacentHTML("beforeend", html);
+		}
+
+		notesListContainer.querySelectorAll(".notes__List-item").forEach(notesListItem => {
+			notesListItem.addEventListener("click", () => {
+				this.onNoteSelect(noteListItem.dataset.noteId);
+			});
+		});
+	}
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,20 @@
 import NotesAPI from './NotesAPI.js';
+import NotesView from './NotesView.js';
 
-NotesAPI.saveNotes({
-	"title": "New Note",
-	"body": "I am a new note"
+const app = document.getElementById("app");
+const view = new NotesView(app, {
+	onNoteSelect(id) {
+		console.log("Note has been selected " + id);
+	}
+	, onNoteAdd() {
+		console.log("Add a note");
+	}
+	, onNoteEdit(newTitle, newBody) {
+		console.log(newTitle, newBody);
+	}
+	, onNoteDelete() {
+		console.log("Delete");
+	}
 });
+
+view.udpateNoteList(NotesAPI.getAllNotes());
